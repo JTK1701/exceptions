1ccd705b072e42767015e53f13f7601ff8df1739
JTK1701 <jonmalkon@mail.ru>
2024-09-28 06:14:09 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ud("JK_jhk1", "GhrT675_9", "GhrT675_");
+        } catch (WrongLoginException wl) {
+            System.out.println(wl.getMessage());
+        } catch (WrongPasswordException wp) {
+            System.out.println(wp.getMessage());
+        }
     }
+
+    private static void ud(String login, String password, String confirmPassword) {
+        if (!login.matches("([a-zA-Z\\d_]*){1,20}")) {
+            throw new WrongLoginException(login);
+        }
+        if (!password.matches("([a-zA-Z\\d_]*){1,20}")) {
+            throw new WrongPasswordException(password);
+        }
+        if (!confirmPassword.equals(password)) {
+            throw new WrongLoginException(confirmPassword);
+        }
+    }
+
 }
